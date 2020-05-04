7e756a0363ced509d93879065d8129c2ff504932
Guillaume Huysmans <ghuysmans99@gmail.com>
2020-05-04 21:56:22 +0200

    Append to a hidden list

--- index.php
+++ index.php
@@ -3,31 +3,43 @@
 require_once 'vendor/autoload.php';
 use Opis\Closure\SerializableClosure;
 //SerializableClosure::setSecretKey("secret");
-$password = "secret";
+
+function dump($l) {
+	?><ul><?php foreach ($l as $x) {?><li><?=$x?></li><?php }?></ul><?php
+}
+
+function step($cur, $cont) {
+	$w = new SerializableClosure(function () use ($cur, $cont) {
+		if (empty($_POST['x']))
+			$cont($cur);
+		else {
+			$cur[] = $_POST['x'];
+			step($cur, $cont);
+		}
+	});
+	$s = serialize($w);
+?>
+<p>Here's the continuation closure encoded in a hidden form input:</p>
+<pre><?=htmlentities($s)?></pre>
+<form method="post">
+<input type="hidden" name="k" value="<?=htmlspecialchars($s, ENT_QUOTES)?>">
+<p>Please don't run your own code.</p>
+<?php //dump($cur); ?>
+<label>
+	Write something, or an empty string to stop:
+	<input name="x" autofocus>
+</label>
+</form>
+<?php
+}
+
 
 if (isset($_REQUEST['k'])) {
 	$k = str_replace("\r", '', $_REQUEST['k']); //added by the browser
 	unserialize($k)();
 }
-else {
-	$t = time();
-	$answer = rand(1, 10);
-	$f = function() use ($t, $answer, &$password) {
-		//hello there
-		$d = date(DATE_RFC2822, $t);
-?>
-<p>On <?=$d?>, you entered <?=$_POST['x']?>.</p>
-<p><?=$_POST['x'] == $answer ? '<b>You won</b>' : 'You lost.'?></p>
-<?php
-	};
-	$w = new SerializableClosure($f);
-	$s = serialize($w);
-?>
-<pre><?=htmlentities($s)?></pre>
-<form method="post">
-<input type="hidden" name="k" value="<?=htmlspecialchars($s, ENT_QUOTES)?>">
-<p>Please guess a number from 1 to 10:</p>
-<label>Guess: <input name="x" type="number"></label>
-</form>
-<?php
-}
+else
+	step(array(), function($l) {
+		?><p>Here's what you've just entered:</p><?php
+		dump($l);
+	});
